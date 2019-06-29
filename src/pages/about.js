e3983eda65d7c9ed1d25c3545c38d59e8c5a4dc8
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-06-29 13:44:17 +0200

    content and layout styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,18 +5,25 @@ import Layout from "../components/layout"
 import Section from "../components/section"
 
 const DefList = styled.dl`
-  dt::after {
-    content: ":";
-  }
-  @media (min-width: 768px) {
-    dt {
-      float: left;
-      clear: left;
-      text-align: right;
-      width: 7rem;
+  div {
+    margin-bottom: 1rem;
+    dt::after {
+      content: ":";
     }
     dd {
-      margin: 0 0 0.15rem 7.5rem;
+      margin-bottom: 0.15rem;
+    }
+
+    @media (min-width: 768px) {
+      dt {
+        float: left;
+        clear: left;
+        text-align: right;
+        width: 7rem;
+      }
+      dd {
+        margin: 0 0 0.15rem 7.5rem;
+      }
     }
   }
 `
@@ -32,41 +39,14 @@ const Link = styled.a`
 const AboutPage = () => (
   <Layout>
     <Section>
-      <h2>About RüDev</h2>
+      <h2>About</h2>
       <p>
-        RüDev is a web development company in Berlin that helps people get the
-        most from contemporary web development.
+        Ryan O'Shea is a trained web developer living and working in Berlin.
       </p>
       <p>
-        Ryan James O'Shea started RüDev after forking his edtech career, and
-        studying web dev elopment.
-      </p>
-      <p>
-        Rüdev works with small businesses, the self-employed, or anyone who
-        wants to get more from the internet and web development.
-      </p>
-      <p>
-        We love to explore problems, find hidden opportunites, and then do
-        killer projects that make people and work better, and smile.
-      </p>
-      <p>
-        So, let's talk about your website, or your data layer, or your sense of
-        being overwhelmed with choices, and make it work better.
-      </p>
-      <p>
-        We love doing this. We want you to enjoy the experience as much as we
-        do!
-      </p>
-    </Section>
-    <Section>
-      <h2>About Ryan</h2>
-      <p>
-        Ryan James O'Shea is a trained web developer living and working in
-        Berlin.
-      </p>
-      <p>
-        He develops software because it gives him such a kick when he solves a
-        problem and it helps someone else with their goals.
+        He loves learning and fixing things. Web development gives him daily
+        satisfaction since there is always something broken, buggy or new to
+        learn.
       </p>
       <p>
         Before web development, Ryan had a 12 year career in edtech with&nbsp;
@@ -88,8 +68,16 @@ const AboutPage = () => (
         as a manager and trainer.
       </p>
       <p>
-        He also has a education in architectural design from the University of
-        Strathcylde in Glasgow.
+        He also has a education and training in architectural design from
+        the&nbsp;
+        <Link
+          href="https://www.strath.ac.uk/engineering/architecture/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          University of Strathcylde
+        </Link>{" "}
+        in Glasgow.
       </p>
       <p>
         <b>Fun fact</b>: if he's running a workshop with a warm-up where the
@@ -123,18 +111,72 @@ const AboutPage = () => (
     <Section>
       <h2>Tech Stack</h2>
       <DefList>
-        <dt>Core</dt>
-        <dd>HTML5, CSS3, Javascript</dd>
-        <dt>Client-side</dt>
-        <dd>SASS, Bootstrap 4, styled-components, React, Redux</dd>
-        <dt>Server-side</dt>
-        <dd>Node, Express, MongoDB, Mongoose</dd>
-        <dt>Dev Tools</dt>
-        <dd>Ubuntu, MacOS, VSCode, Bash, Git, Git Kracken</dd>
-        <dt>Deployment</dt>
-        <dd>Gatsby, Netlify, Now, Next</dd>
-        <dt>Methods</dt>
-        <dd>Scrum, Kanban, Wardley Maps</dd>
+        <div>
+          <dt>Core</dt>
+          <dd>HTML5, CSS3, Javascript</dd>
+        </div>
+        <div>
+          <dt>Client-side</dt>
+          <dd>SASS, Bootstrap 4, styled-components, React, Redux</dd>
+        </div>
+        <div>
+          <dt>Server-side</dt>
+          <dd>Node, Express, MongoDB, Mongoose</dd>
+        </div>
+        <div>
+          <dt>Dev Tools</dt>
+          <dd>Ubuntu, MacOS, VSCode, Bash, Git, Git Kracken</dd>
+        </div>
+        <div>
+          <dt>Deployment</dt>
+          <dd>Gatsby, Netlify, Now, Next</dd>
+        </div>
+        <div>
+          <dt>Methods</dt>
+          <dd>Scrum, Kanban, Wardley Maps</dd>
+        </div>
+      </DefList>
+    </Section>
+    <Section>
+      <h2>Resume</h2>
+      <p>
+        This is the short version of Ryan's career and is optimised to support
+        your decision making process. For more depth see the&nbsp;
+        <Link
+          href="https://exitroute.github.io/resume/?token=1973"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume App
+        </Link>{" "}
+        or his{" "}
+        <Link
+          href="https://linkedin.com/in/ryanjamesoshea/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </Link>{" "}
+        page.
+      </p>
+      <DefList>
+        <div>
+          <dt>Development</dt>
+          <dd>July 2018 - July 2019</dd>
+          <dd>Web Development Trainee</dd>
+          <dd>Digital Career Institute, Berlin</dd>
+        </div>
+        <div>
+          <dt>Management</dt>
+          <dd>September 2005 - December 2016</dd>
+          <dd>School Manager</dd>
+          <dd>Wall Street Institute, Berlin</dd>
+        </div>
+        <div>
+          <dt>Design</dt>
+          <dd>October 1994 - June 1999</dd>
+          <dd>BSc Architectural Studies</dd>
+        </div>
       </DefList>
     </Section>
   </Layout>
