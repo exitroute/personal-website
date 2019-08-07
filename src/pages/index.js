656c90ca31c61a98dcad56477dc9c512dc8552a6
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-08-07 10:05:08 +0200

    style tweak

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,35 @@
 import React from "react"
 import styles from "./index.module.css"
-import AniLink from "gatsby-plugin-transition-link/AniLink";
-
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const IndexPage = props => (
   <main>
     <div className={styles.wrapper}>
       <div className={styles.siteTitle}>
         <h1>Ryan O'Shea</h1>
-        <p className={styles.tags}>#development<br />#management #design<br />#berlin</p>
+        <p>
+          #development
+          <br />
+          #management #design
+          <br />
+          #berlin
+        </p>
       </div>
       <ul className={styles.nav}>
         <li>
-          <AniLink fade to="/about" duration={0.2} >About</AniLink>
+          <AniLink fade to="/about" duration={0.2}>
+            About
+          </AniLink>
         </li>
         <li>
-          <AniLink fade to="/projects" duration={0.2} >Projects</AniLink>
+          <AniLink fade to="/projects" duration={0.2}>
+            Projects
+          </AniLink>
         </li>
         <li>
-          <AniLink fade to="/blog" duration={0.2} >Blog</AniLink>
+          <AniLink fade to="/blog" duration={0.2}>
+            Blog
+          </AniLink>
         </li>
       </ul>
     </div>
