11e66ce18f141a51c5fe798aaf5c3c5ab3bb469b
Ryan O'Shea <ryan.oshea@exitroute.org>
2020-04-22 21:32:11 +0200

    add seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,37 @@
 import React from "react"
 import styles from "./index.module.css"
 import Link from "gatsby-link"
+import SEO from "../components/SEO"
 
 const IndexPage = props => (
-  <main>
-    <div className={styles.wrapper}>
-      <div className={styles.siteTitle}>
-        <h1>Ryan O'Shea</h1>
-        <p>
-          #development
-          <br />
-          #management #design
-          <br />
-          #berlin
-        </p>
+  <>
+    <SEO />
+    <main>
+      <div className={styles.wrapper}>
+        <div className={styles.siteTitle}>
+          <h1>Ryan O'Shea</h1>
+          <p>
+            #development
+            <br />
+            #management #design
+            <br />
+            #berlin
+          </p>
+        </div>
+        <ul className={styles.nav}>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
       </div>
-      <ul className={styles.nav}>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-      </ul>
-    </div>
-  </main>
+    </main>
+  </>
 )
 
 export default IndexPage
